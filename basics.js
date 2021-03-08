fb74467b576a7b935885860089a2f3a7deef1e0f
tomw268 <tomw268@gmail.com>
2021-03-08 11:17:17 -0500

    Added how to declare variables

--- basics.js
+++ basics.js
@@ -1,3 +1,16 @@
 let hello = 'Hello World!!!';
-
 console.log(hello);
+
+// 3 ways to declare variables
+
+var a = 1;
+
+a = 2;
+console.log(a);
+
+let b = 3;
+b = 4;
+console.log(b);
+
+const c = 1;
+console.log(c);
