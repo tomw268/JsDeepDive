0310a21ec14de6229f320faa808d9be45f0ea83e
tomw268 <tomw268@gmail.com>
2021-04-05 09:53:18 -0400

    worked on function declarations and function expressions

--- basics.js
+++ basics.js
@@ -483,23 +483,43 @@
 // }
 
 // SWITCH STATEMENTS
-const grade = 'D';
-switch (grade) {
-  case 'A':
-    console.log('You got and A!');
-    break;
-  case 'B':
-    console.log('You got and B!');
-    break;
-  case 'C':
-    console.log('You got and C!');
-    break;
-  case 'D':
-    console.log('You got and D!');
-    break;
-  case 'E':
-    console.log('You got and E!');
-    break;
-  default:
-    console.log('Not a valid grade');
+// const grade = 'D';
+// switch (grade) {
+//   case 'A':
+//     console.log('You got and A!');
+//     break;
+//   case 'B':
+//     console.log('You got and B!');
+//     break;
+//   case 'C':
+//     console.log('You got and C!');
+//     break;
+//   case 'D':
+//     console.log('You got and D!');
+//     break;
+//   case 'E':
+//     console.log('You got and E!');
+//     break;
+//   default:
+//     console.log('Not a valid grade');
+// }
+
+// FUNCTION & METHODS
+
+// FUNCTION DECLARATIONS & EXPRESSIONS
+// FIUNCTION DECLARATION
+
+function greet() {
+  console.log('hello there');
 }
+greet();
+greet();
+greet();
+
+/// FUNCTION EXPRESSION
+
+const speak = function () {
+  console.log('GDay');
+};
+
+speak();
