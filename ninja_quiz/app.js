efea1fc76d2359ea15229ffb33992a80fc082f5b
tomw268 <tomw268@gmail.com>
2021-06-17 07:06:54 -0700

    Adding in the js to the site to make it dynamic

---
+++ ninja_quiz/app.js
@@ -0,0 +1,8 @@
+const correctAnswers = ['A', 'B', 'B', 'B'];
+
+const form = document.querySelector('.quiz-form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  let score = 0;
+});
