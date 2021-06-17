3a66998473207ab979d9050b91709f2ed8b38f03
tomw268 <tomw268@gmail.com>
2021-06-17 07:16:09 -0700

    had to do a little debugging

--- ninja_quiz/app.js
+++ ninja_quiz/app.js
@@ -5,4 +5,17 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   let score = 0;
+  const userAnswers = [
+    form.q1.value,
+    form.q2.value,
+    form.q3.value,
+    form.q4.value,
+  ];
+  // check answers
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswers[index]) {
+      score += 25;
+    }
+  });
+  console.log(score);
 });
